eff83156aae42dd46d3ac82393e2ba9bcf8de3cc
ferraribilly.git <williamferrari52@gmail.com>
2025-11-13 16:18:59 -0300

    %9

--- view.py
+++ view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from datetime import datetime, timezone, timedelta
 from model import Usuario
 from model import get_db_status
