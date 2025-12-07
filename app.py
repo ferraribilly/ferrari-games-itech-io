66cbd4bf58e412a90e4220668343ec54bd2b907d
* <williamferrari52@gmail.com>
2025-12-07 12:24:34 -0300

    atualizacaowill1eekjh

--- app.py
+++ app.py
@@ -1314,5 +1314,10 @@ def list_mongo_collections():
 #===========================================
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True)
-
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port,
+        debug=True,
+        allow_unsafe_werkzeug=True
+    )
